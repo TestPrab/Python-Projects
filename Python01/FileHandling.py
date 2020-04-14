62cbdabc08444f47af8384dce86503f801513052
TestPrab <59395410+TestPrab@users.noreply.github.com>
2020-04-14 18:41:40 +0530

    Update FileHandling.py

--- Python01/FileHandling.py
+++ Python01/FileHandling.py
@@ -5,7 +5,6 @@ def search(filename,dirname):
        if filename in files:
            pathf.append(os.path.join(root, filename))
            return pathf
-print("Enter the filename and the directory")
 filename=input()
 dirname=input()
 print(search(filename,dirname))
