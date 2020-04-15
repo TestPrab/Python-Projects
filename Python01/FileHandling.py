9a3fb9c406233f89a882d17ef8fa524db8bdd5de
TestPrab <59395410+TestPrab@users.noreply.github.com>
2020-04-15 18:06:45 +0530

    Update FileHandling.py

--- Python01/FileHandling.py
+++ Python01/FileHandling.py
@@ -1,10 +1,20 @@
 import os
+st="File Not Found"
 def search(filename,dirname):
    pathf=[]
    for root,dir,files in os.walk(dirname):
-       if filename in files:
-           pathf.append(os.path.join(root, filename))
-           return pathf
+        for inFile in files:
+           if filename==inFile:
+             pathf.append(os.path.join(root, filename))
+             return pathf
+        for idir in dir:
+            inDir=os.path.join(root,idir)
+            inPath=search(filename,inDir)
+            if(inPath!=0):
+                return inPath
+            else:
+                return st
+        
 filename=input()
 dirname=input()
 print(search(filename,dirname))
