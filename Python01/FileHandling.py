4a35efc484c0667383131bcb65c7b1525f8e3685
TestPrab <59395410+TestPrab@users.noreply.github.com>
2020-04-15 22:18:28 +0530

    Update FileHandling.py
    Now it can search for directories as well

--- Python01/FileHandling.py
+++ Python01/FileHandling.py
@@ -2,19 +2,30 @@ import os
 st="File Not Found"
 def search(filename,dirname):
    pathf=[]
-   os.chdir(str(dirname))
+   #os.chdir(str(dirname))
    for root,dir,files in os.walk(dirname):
+
+        print(" Printing the files : ",files)
         for inFile in files:
-           if filename==inFile:
+            if filename==inFile:
              pathf.append(os.path.join(root, filename))
              return pathf
+             break
+        for indir in dir:
+            print("Entering this one ")
+            if filename==indir:
+                pathf.append(os.path.join(root,indir))
+                return pathf
         for idir in dir:
+            print("Printing the directory : ",idir)
             inDir=os.path.join(root,idir)
             inPath=search(filename,inDir)
             if(inPath!=0):
                 return inPath
             else:
                 return st
+
+        
         
 filename=input()
 dirname=input()
